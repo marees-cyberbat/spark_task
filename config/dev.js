c9a0aef01db376e4b8a4b3c177b30ef3a75f3547
marees-cyberbat <65803019+marees-cyberbat@users.noreply.github.com>
2020-05-30 21:36:48 +0530

    Delete dev.js

--- config/dev.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    mongoURI: 'mongodb+srv://marees:marees2020@mars-v6pyi.mongodb.net/spark_notes?retryWrites=true&w=majority',
-    jwtKey: '@&*(*&BCIW)hey$&%^#^my$Secret$Jwt$key&%*)'
-};
-
-
- 
